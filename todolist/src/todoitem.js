e0264741dd8aa982c7085a6798fbdcb981ddbd2e
VictoryMao <1415088671@qq.com>
2019-03-04 20:00:10 +0800

    todolist的增和删

--- todolist/src/todoitem.js
+++ todolist/src/todoitem.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
 
 class TodoItem extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
-        // 指向todoitem, 说是这种写法比较好
         this.handleClick = this.handleClick.bind(this);
     }
-
     render () {
         const { content } = this.props
-        return (
-            <div
-                onClick={this.handleClick}
-                dangerouslySetInnerHTML={{__html: content}}
-            >
-            </div>
-        )
+        return <div onClick={this.handleClick}>{content}</div>
     }
 
     handleClick () {
         const { deleteItem, index } = this.props
-        console.log(deleteItem)
         deleteItem(index)
     }
 }
