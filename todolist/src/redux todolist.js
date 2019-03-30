1bc66ac04a63a8e8ca20f2a847815936ee9401e1
VictoryMao <1415088671@qq.com>
2019-03-30 17:26:40 +0800

    react 无状态组件

--- todolist/src/redux todolist.js
+++ todolist/src/redux todolist.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
-
+import 'antd/dist/antd.css';
+import store from './store';
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators';
+import TodoListUI from './todoListUI';
 class TodoList extends Component {
+    constructor (props) {
+        super(props);
+        this.state = store.getState();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        this.handleItemDelete = this.handleItemDelete.bind(this);
+        store.subscribe(this.handleStoreChange);
+    }
+
     render () {
-        return <div>hello world</div>
+        return (<TodoListUI
+                    inputValue={this.state.inputValue}
+                    list={this.state.list}
+                    handleInputChange={this.handleInputChange}
+                    handleBtnClick={this.handleBtnClick}
+                    handleItemDelete={this.handleItemDelete}
+                />)
+    }
+
+    handleInputChange (e) {
+        store.dispatch(getInputChangeAction(e.target.value))
+    }
+
+    handleStoreChange () {
+        this.setState(store.getState());
+    }
+
+    handleBtnClick () {
+        store.dispatch(getAddItemAction())
+    }
+
+    handleItemDelete (index) {
+        store.dispatch(getDeleteItemAction(index))
     }
 }
 
---
+++ todolist/src/todoListUI.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Input, Button, List } from 'antd';
+
+//  无状态组件， 性能较高
+const TodoListUI = (props) => {
+    return (
+        <div>
+            <Input placeholder="Basic usage"
+            value={props.inputValue}
+            style={{width:300, marginRight:30, marginBottom:20}}
+            onChange={props.handleInputChange}
+        />
+            <Button type="primary" onClick={props.handleBtnClick}>提交</Button>
+            <List
+                style={{width: 300}}
+                header={<div>是的，我是头部</div>}
+                footer={<div>没错，我是底部</div>}
+                bordered
+                dataSource={props.list}
+                renderItem={(item, index) => (<List.Item onClick={() => {props.handleItemDelete(index)}}>{item}</List.Item>)}
+            >
+            </List>
+        </div>
+    )
+}
+// class TodoListUI extends Component {
+//     render () {
+//         return (
+//             <div>
+//                 <Input placeholder="Basic usage"
+//                     value={this.props.inputValue}
+//                     style={{width:300, marginRight:30, marginBottom:20}}
+//                     onChange={this.props.handleInputChange}
+//                  />
+//                 <Button type="primary" onClick={this.props.handleBtnClick}>提交</Button>
+//                 <List
+//                     style={{width: 300}}
+//                     header={<div>是的，我是头部</div>}
+//                     footer={<div>没错，我是底部</div>}
+//                     bordered
+//                     dataSource={this.props.list}
+//                     renderItem={(item, index) => (<List.Item onClick={() => {this.props.handleItemDelete(index)}}>{item}</List.Item>)}
+//                 >
+//                 </List>
+//             </div>
+//         )
+//     }
+// }
+
+export default TodoListUI;
